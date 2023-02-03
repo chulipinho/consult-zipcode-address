d9fd39232ac391a2e32db45f1d4f047b6f9f4f4f
chulipinho <fellipe.luz.machado@gmail.com>
2023-02-03 18:01:33 -0300

    Additional files

---
+++ src/main/java/com/example/consultaFrete/ConsultaFreteApplication.java
@@ -0,0 +1,13 @@
+package com.example.consultaFrete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultaFreteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultaFreteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/consultaFrete/ConsultaFreteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.consultaFrete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultaFreteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
