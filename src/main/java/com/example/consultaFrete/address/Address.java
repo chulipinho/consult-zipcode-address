105a10a4f957ebbe431beecdd8182ef8a9284fcc
chulipinho <fellipe.luz.machado@gmail.com>
2023-02-05 16:24:49 -0300

    Creates factory ResponseAddress constructor

--- src/main/java/com/example/consultaFrete/address/Address.java
+++ src/main/java/com/example/consultaFrete/address/Address.java
@@ -9,20 +9,33 @@ import lombok.Getter;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Address {
+    @JsonProperty("cep") 
     @Getter private String zipcode;
+
+    @JsonProperty("rua") @JsonAlias("logradouro") 
     @Getter private String street;
+
+    @JsonProperty("complemento") 
     @Getter private String complement;
+
+    @JsonProperty("bairro") 
     @Getter private String district;
+
+    @JsonProperty("cidade") @JsonAlias("localidade") 
     @Getter private String city;
+    
+    @JsonProperty("estado") @JsonAlias("uf") 
     @Getter private State state;
     
+    public Address() {}
+
     public Address(
-        @JsonProperty("cep") String zipcode, 
-        @JsonProperty("rua") @JsonAlias("logradouro") String street, 
-        @JsonProperty("complemento") String complement, 
-        @JsonProperty("bairro") String district, 
-        @JsonProperty("cidade") @JsonAlias("localidade") String city, 
-        @JsonProperty("estado") @JsonAlias("uf") String state
+        String zipcode, 
+        String street, 
+        String complement, 
+        String district, 
+        String city, 
+       String state
     ) {
         this.zipcode = zipcode;
         this.street = street;
--- src/main/java/com/example/consultaFrete/address/ResponseAddress.java
+++ src/main/java/com/example/consultaFrete/address/ResponseAddress.java
@@ -1,15 +1,33 @@
 package com.example.consultaFrete.address;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.Getter;
 
 public class ResponseAddress extends Address{
-
+    @JsonProperty("frete")
     @Getter private double deliveryFee;
+
+    public ResponseAddress() {
+        super();
+    }
 
     public ResponseAddress(String zipcode, String street, String complement, String district, String city,
             String state, double deliveryFee) {
         super(zipcode, street, complement, district, city, state);
 
         this.deliveryFee = deliveryFee;
+    }
+
+    public static ResponseAddress fromAddress(Address address, double deliveryFee) {
+        return new ResponseAddress(
+            address.getZipcode(),
+            address.getStreet(),
+            address.getComplement(),
+            address.getDistrict(),
+            address.getCity(),
+            address.getState().toString(),
+            deliveryFee
+        );
     }
 }
