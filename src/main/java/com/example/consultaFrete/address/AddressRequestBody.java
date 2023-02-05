c731bbb3bf1abe6ac7a0f2a7b7dff94051b9a3da
chulipinho <fellipe.luz.machado@gmail.com>
2023-02-05 16:25:56 -0300

    Treats input cases

--- src/main/java/com/example/consultaFrete/address/AddressRequestBody.java
+++ src/main/java/com/example/consultaFrete/address/AddressRequestBody.java
@@ -4,15 +4,34 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class AddressRequestBody {
     @Getter 
     @NotBlank
+    @Size(max = 8, message = "Por favor insira um CEP válido")
     private String cep;
 
     public AddressRequestBody(@JsonProperty("cep") String cep) {
-        this.cep = cep;
+        String cleanZipcode = cep.replaceAll("[^\\d]", "");
+
+        this.cep = padLeftZeros(cleanZipcode, 8);
+        String p = padLeftZeros(cleanZipcode, 8);
+        System.out.println(p);
+    }
+
+    private String padLeftZeros(String inputString, int length) {
+        if (inputString.length() >= length) {
+            return inputString;
+        }
+        StringBuilder sb = new StringBuilder();
+        while (sb.length() < length - inputString.length()) {
+            sb.append('0');
+        }
+        sb.append(inputString);
+    
+        return sb.toString();
     }
 }
